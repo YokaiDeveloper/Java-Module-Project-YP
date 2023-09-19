9f30c5af8b14680002e73f27bded8b388f70a90e
Victor <Qoptt@yandex.ru>
2023-09-19 20:16:43 +0300

    Создание метода getNumber и подсчет кол-ва человек

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
 
 public class Main {
+    public static int getNumber(Scanner scanner) {
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                if (number > 1) {
+                    return number;
+                } else {
+                    System.out.println("Указано неверное значение");
+                }
+            } else {
+                System.out.println("Указано неверное значение. Пожалуйста, введите целое " +
+                        "число больше 1.");
+                scanner.next();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("На какое кол-во человек необходимо разделить счет? :)");
+
+        int countHuman = getNumber(scanner);
+        System.out.println("Количество людей: " + countHuman);
     }
 }
